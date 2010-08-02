efafd8ae26eca9010f83020570d6f3abae01659e
Josh Charles <josh.charles@gmail.com>
2010-08-02 18:28:02 -0500

    Removed unecessary functions from Message class

--- RooChat.Models/Message.cs
+++ RooChat.Models/Message.cs
@@ -14,21 +14,6 @@ namespace RooChat.Models
                     where messages.Id > message_id && messages.Chatroom.Url == chat_url
                     select messages).ToList();
         }
-        public static List<Message> FetchAfter(int message_id, int conversation_id)
-        {
-            var db = new RooChatDataContext();
-            return (from messages in db.Messages
-                    where messages.Id > message_id && messages.Chat_id == conversation_id
-                    select messages).ToList();
-        }
-        public static List<Message> FetchFor(int conversation_id)
-        {
-            var db = new RooChatDataContext();
-            return (from messages in db.Messages
-                    where messages.Chat_id == conversation_id
-                    orderby messages.Id ascending
-                    select messages).ToList();
-        }
         public static Message Find(int message_id)
         {
             var db = new RooChatDataContext();
