7a84bb35fc1aea0ad95b85abedbfaa68c623ad9b
Josh Charles <charlesj@umkc.edu>
2010-08-12 15:27:37 -0500

    Tests stuff.

---
+++ RooChat.Tests/Controllers/ChatroomsControllerTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RooChat;
+using RooChat.Models;
+
+namespace RooChat.Tests.Controllers
+{
+    [TestClass]
+    class ChatroomsControllerTest
+    {
+    }
+}
