a2b81404e7ec5164d933c1127ac94ef304881e85
Josh Charles <josh.charles@gmail.com>
2012-06-03 17:46:09 -0700

    Finalized fixing robots.txt

--- SimpleChat/Global.asax.cs
+++ SimpleChat/Global.asax.cs
@@ -18,7 +18,7 @@ namespace SimpleChat
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-            routes.IgnoreRoute("robots.txt");
+
             routes.MapRoute(
                 "FetchMessages",
                 "chatrooms/FetchMessages/{chat_url}/{last_m_id}",
